540762d038e083e1b53a7121a78700ae332bee5a
廖楷 <liaokai@liaokaideMacBook-Air.local>
2016-01-20 16:27:09 +0800

    second commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,14 @@
 package com.company;
 
+
 public class Main {
 
     public static void main(String[] args) {
-	// writegit  your code here
+        // writegit  your code here
+        int ATTACK = 300, DEFEND = 150;
+        int hurt = (ATTACK-DEFEND);
+        System.out.print("Hurt:"+hurt);
+
     }
 }
 
