97346652e2e7c11f7f8856fde6a1b27f3f3a5205
stevenliaokai <1113562386@qq.com>
2016-01-20 18:20:57 +0800

    added several game rules

--- src/com/company/Hero.java
+++ src/com/company/Hero.java
@@ -10,21 +10,29 @@ public class Hero {
     public int def;
     public String weapon;
     public int health;
+    public int shield;
+    public boolean isBoss;
 
-    public Hero(String firstName, String lastName, int attDmg, int def){
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.attDmg = attDmg;
-        this.def = def;
-        this.weapon = "";
-        this.health = 500;
-    }
-    public Hero(String firstName, String lastName, int attDmg, int def, String weapon, int health){
+    public Hero(String firstName, String lastName, int attDmg, int def, String weapon, int health, int shield, boolean isBoss) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.attDmg = attDmg;
         this.def = def;
         this.weapon = weapon;
         this.health = health;
+        this.shield = shield;
+        this.isBoss = isBoss;
+    }
+
+    public void Attack(Hero defender) {
+        if (defender.shield > 0) {
+            defender.shield--;
+        } else {
+            int attDmgThisTime = this.attDmg;
+            if (this.isBoss && this.health < 200) {
+                attDmgThisTime = this.attDmg * 3;
+            }
+            defender.health -= attDmgThisTime - defender.def;
+        }
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,18 +4,15 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        Hero Steven = new Hero("Steven", "Liao", 35, 20, "shuangdao", 600);
-        Hero Max = new Hero("Max", "Zeng", 30, 15, "", 500);
-        int i = 0;
-        while(i < 10){
-            i++;
-            Attack(Max, Steven);
+        Hero Steven = new Hero("Steven", "Liao", 35, 20, "shuangdao", 600, 1, false);
+        Hero Max = new Hero("Max", "Zeng", 30, 15, "", 500, 10, true);
+        while(Max.health > 0 && Steven.health > 0) {
+            Max.Attack(Steven);
+            Steven.Attack(Max);
         }
-        System.out.print(Steven.health);
+        System.out.println("Max health : "  + Max.health);
+        System.out.println("Steven health : " + Steven.health);
     }
 
-    public static void Attack(Hero Attacker, Hero defender){
-        defender.health -= Attacker.attDmg - defender.def;
-    }
 }
 
